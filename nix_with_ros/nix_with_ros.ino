7ec8488eb45480c6697ee891bb4e41a5b082276f
gregorthebigmac <gregorthebigmac@gmail.com>
2019-06-06 16:15:10 -0500

    for some reason, my last changes didn't make it.

--- nix_with_ros/nix_with_ros.ino
+++ nix_with_ros/nix_with_ros.ino
@@ -1,3 +1,17 @@
-void setup() {}
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <Sabertooth.h>
 
-void loop() {}
+int x = 0;
+
+void setup() {
+	Serial.begin(9600);
+	while (!Serial);
+}
+
+void loop() {
+	Serial.print("test");
+	Serial.println(x);
+	x++;
+	delay(100);
+}
