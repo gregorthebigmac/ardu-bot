d785774ebc6c95ba1d3c5ca982f8c4472e77ece0
gregorthebigmac <gregorthebigmac@gmail.com>
2019-06-18 12:01:37 -0500

    added another arduino as subscriber

---
+++ nix_with_ros/arduino_subscriber.ino
@@ -0,0 +1,24 @@
+#include <ros.h>
+#include <std_msgs/String.h>
+
+ros::NodeHandle nh;
+
+void messageCb(const std_msgs::String& msg) {
+	if (msg.data == "ON")
+		digitalWrite(13, HIGH-digitalRead(13));	// blink the LED
+	else
+		digitalWrite(13, LOW-digitalRead(13));	// turn off the LED
+}
+
+ros::Subscriber sub("LED", &messageCb);
+
+void setup() {
+	pinMode(13, OUTPUT);
+	nh.initNode();
+	nh.subscribe(sub);
+}
+
+void loop() {
+	nh.spinOnce();
+	delay(1000);
+}
